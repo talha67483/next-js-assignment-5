f6d433408459a5e0a0ab5be7b387ee4b1d585a5b
talha67483 <mh2971966@gmail.com>
2024-11-13 09:00:49 -0800

    added file

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <header className='bg-[#A29875] h-[134px] w-full'>
-            <h1 className='w-[439px] h-[94px] text-[75px] font-bold text-[#FFFFFF] mx-7 p-3'>MANZZARI</h1>
+        <header className='bg-[#A29875] h-[134px] w-full '>
+            <h1 className='w-[439px] h-[94px] text-[75px] font-bold text-[#FFFFFF] md:mx-7 mx-32 p-3'>MANZZARI</h1>
 
         </header>
       
--- components/Hero_Section.tsx
+++ components/Hero_Section.tsx
@@ -4,19 +4,21 @@ import Image from 'next/image'
 const Hero_Section = () => {
   return (
     <div>
-        <main className='flex'>
-            <div className='w-1/2 flex flex-col justify-center ml-16'>
-                <h1 className='w-[495px] h-[189px] font-bold text-[40px]'>IMPECCABLE CRAFTSMANSHIP AND FINESSE
+        <main className="flex flex-col md:flex-row ">
+            <div className=' md:w-1/2 w-full flex  flex-col justify-center ml-16'>
+                <h1 className= ' w-[350px] md:w-[495px] mx-28 my-10 md:mx-0 md:my-3 h-[189px] font-bold text-[40px]'>IMPECCABLE CRAFTSMANSHIP AND FINESSE
                 </h1>
-                <p className='text-[30px] w-[802px] h-[147px] font-medium text-[#787054] mt-4'>
+                <p className='text-[30px]  md:w-[802px] w-[600px] h-[140px] md:h-[147px] font-medium text-[#787054] mt-4'>
                 An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.
                 </p>
-                <button className='w-[288px] h-[58px] bg-[#A29875] rounded-lg mt-4 ml-5 text-white font-medium text-center text-[30px] hover:bg-[#d4c696] hover:text-slate-800'>Explore Now</button>
+                <button className=' mx-32 w-[288px] h-[58px] bg-[#A29875] rounded-lg mt-4 md:ml-5 text-white font-medium text-center text-[30px] hover:bg-[#d4c696] hover:text-slate-800'>Explore Now</button>
 
                 
             </div>
-            <div className='w-1/2 flex justify-center my-20 ml-20'>
-                <Image src={"/images/figma-image.jpeg"} alt='Figma Image' width={442} height={611}/>
+            <div className=' md:w-1/2 w-full md:flex md:justify-center  md:my-20 md:ml-20'>
+                <Image src={"/images/figma-image.jpeg"} alt='Figma Image'   width={442} height={611}
+                className="inline-block ml-32 "
+                />
 
 
             </div>
@@ -27,3 +29,6 @@ const Hero_Section = () => {
 }
 
 export default Hero_Section
+
+
+
